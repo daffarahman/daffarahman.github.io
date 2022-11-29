88b1e7ea02d494156271b4bbd4a4a183eb7712ae
daffarahman <69158099+daffarahman@users.noreply.github.com>
2022-11-29 22:39:55 +0700

    added vue test

---
+++ components/my-component.js
@@ -0,0 +1,7 @@
+// my-component.js
+export default {
+  data() {
+    return { message: "My Vue Website!" };
+  },
+  template: `<div>{{ message }}</div>`,
+};
---
+++ index.js
@@ -0,0 +1,4 @@
+import { createApp } from "vue";
+import MyComponent from "./components/my-component.js";
+
+createApp(MyComponent).mount("#app");
